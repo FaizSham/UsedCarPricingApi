e967cb663372378029a7695454074716082d36c1
Muhammad Faizzuddin Bin Shamsul Ariffin <muhammadfaizzuddin.tmnrnd.com.my>
2026-01-28 13:00:27 +0800

    Add Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  (app as any).set('etag', false);
-  app.use((req, res, next) => {
-    res.removeHeader('x-powered-by');
-    res.removeHeader('date');
-    next();
-  });
+  const config = new DocumentBuilder()
+    .setTitle('MyCV API')
+    .setDescription('API documentation')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
 
   const port = Number(process.env.PORT) || 3000;
   await app.listen(port, '0.0.0.0');
 }
-
 bootstrap();
