29be819bc77e99961c4c2c698c20fe954770b26a
Muhammad Faizzuddin Bin Shamsul Ariffin <muhammadfaizzuddin.tmnrnd.com.my>
2026-01-28 15:30:03 +0800

    Fix estimate query for Postgres

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -11,20 +11,20 @@ export class ReportsService {
   constructor(@InjectRepository(Report) private repo: Repository<Report>) {}
 
   createEstimate({ make, model, lng, lat, year, mileage }: GetEstimateDto) {
-    return this.repo
-      .createQueryBuilder()
-      .select('AVG(price)', 'price')
-      .where('make = :make', { make })
-      .andWhere('model = :model', { model })
-      .andWhere('lng - :lng BETWEEN -5 AND 5', { lng })
-      .andWhere('lat - :lat BETWEEN -5 AND 5', { lat })
-      .andWhere('year - :year BETWEEN -3 AND 3', { year })
-      .andWhere('approved IS TRUE')
-      .orderBy('ABS(mileage - :mileage)', 'DESC')
-      .setParameters({ mileage })
-      .limit(3)
-      .getRawOne();
-  }
+  return this.repo
+    .createQueryBuilder('report')
+    .select('AVG(report.price)', 'price')
+    .where('report.make = :make', { make })
+    .andWhere('report.model = :model', { model })
+    .andWhere('report.lng - :lng BETWEEN -5 AND 5', { lng })
+    .andWhere('report.lat - :lat BETWEEN -5 AND 5', { lat })
+    .andWhere('report.year - :year BETWEEN -3 AND 3', { year })
+    .andWhere('report.approved IS TRUE')
+    .orderBy('ABS(report.mileage - :mileage)', 'DESC')
+    .setParameters({ mileage })
+    .limit(3)
+    .getRawOne();
+}
 
   create(reportDto: CreateReportDto, user: User) {
     const report = this.repo.create(reportDto);
