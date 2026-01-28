b4a7e850aa42c64cd8076cc9c8a4374d79da3ec8
Muhammad Faizzuddin Bin Shamsul Ariffin <muhammadfaizzuddin.tmnrnd.com.my>
2026-01-28 15:16:54 +0800

    Add swagger property

--- src/reports/dtos/get-estimate.dto.ts
+++ src/reports/dtos/get-estimate.dto.ts
@@ -7,30 +7,37 @@ import {
   IsLatitude,
 } from 'class-validator';
 import { Transform } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class GetEstimateDto {
+  @ApiProperty()
   @IsString()
   make: string;
 
+  @ApiProperty()
   @IsString()
   model: string;
 
+  @ApiProperty()
   @Transform(({ value }) => parseInt(value))
   @IsNumber()
   @Min(1930)
   @Max(2050)
   year: number;
 
+  @ApiProperty()
   @Transform(({ value }) => parseInt(value))
   @IsNumber()
   @Min(0)
   @Max(1000000)
   mileage: number;
 
+  @ApiProperty()
   @Transform(({ value }) => parseFloat(value))
   @IsLongitude()
   lng: number;
 
+  @ApiProperty()
   @Transform(({ value }) => parseFloat(value))
   @IsLatitude()
   lat: number;
